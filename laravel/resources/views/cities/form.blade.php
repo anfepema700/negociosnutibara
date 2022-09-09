e5e2d786703ef3b79e78d5b8a3649ae0167bdf98
anfepema700 <anfepema700@github.com>
2022-09-09 18:28:07 -0500

    changes finals for update project

--- laravel/resources/views/cities/form.blade.php
+++
@@ -1,7 +0,0 @@
-
-
-<h1>Formulario cities</h1>
-<label for="cityname"> City Name:   </label>
-<input type="text" name="city_name" id="cityname">
-<input type="submit" value="Guardar" class="btn btn-primary">
-</div>
--- pruebatecnica/resources/views/welcome.blade.php
+++ pruebatecnica/resources/views/welcome.blade.php
@@ -7,4 +7,13 @@ Debe crear un sistema de información que permita realizar ordenes depedidos<br>
 
 
 <h3>Ingresa en el menú para navegar por cada dependencia</h3>
+<h5>Modulo <a href="customers">Customers</a><br>Listado de clients y nuevos registros</h5>
+<p>Puedes crear, editar, eliminar y actualizar los registros</p>
+<h5>Modulo <a href="Orders">Orders</a><br>Podras visualizar las ordenes y tomar nuevas</h5>
+<p>Puedes crear, editar, eliminar y actualizar los registros</p>
+<h5>Modulo <a href="Products">Products </a><br>Puedes administrar los productos</h5>
+<p>Puedes crear, editar, eliminar y actualizar los registros</p>
+<h5>Modulo <a href="Cities">Cities</a><br>Encontraras las ciudades registradas. </h5>
+<p>Puedes crear, editar, eliminar y actualizar los registros</p>
+
 </div>
